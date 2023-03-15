26634b0a7fd167821d5d5faa2ef222772501a0fa
anirban00537 <anirban00537@gmail.com>
2023-03-15 13:17:23 +0600

    landing page updated

--- src/components/Landing/Banner/index.tsx
+++ src/components/Landing/Banner/index.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Banner = () => {
   return (
     <div>
-      {' '}
+      {" "}
       <div className="isolate bg-white">
         <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-          <svg
+          {/* <svg
             className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
             viewBox="0 0 1155 678"
             fill="none"
@@ -31,7 +31,7 @@ const Banner = () => {
                 <stop offset={1} stopColor="#FF80B5" />
               </linearGradient>
             </defs>
-          </svg>
+          </svg> */}
         </div>
         <div className="px-6 pt-6 lg:px-8">
           <div>
@@ -199,7 +199,7 @@ const Banner = () => {
                 <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                   <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                     <span className="text-gray-600">
-                      Announcing our next round of funding.{' '}
+                      Announcing our next round of funding.{" "}
                       <a href="#" className="font-semibold text-indigo-600">
                         <span className="absolute inset-0" aria-hidden="true" />
                         Read more <span aria-hidden="true">→</span>
@@ -209,12 +209,14 @@ const Banner = () => {
                 </div>
                 <div>
                   <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-                    Data to enrich your online business
+                    Note-taking made simple - Never miss a thought
                   </h1>
                   <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                    qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                    occaecat fugiat aliqua.
+                    Transform the way you take notes with FlyerNotes.
+                    Effortlessly capture and connect your ideas with our
+                    intuitive note-taking app, and never miss a thought again.
+                    Streamline your productivity and take control of your life -
+                    sign up today.
                   </p>
                   <div className="mt-8 flex gap-x-4 sm:justify-center">
                     <Link href="/dashboard">
@@ -228,19 +230,10 @@ const Banner = () => {
                         </span>
                       </button>
                     </Link>
-                    <a
-                      href="#"
-                      className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                    >
-                      Live demo
-                      <span className="text-gray-400" aria-hidden="true">
-                        →
-                      </span>
-                    </a>
                   </div>
                 </div>
                 <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-                  <svg
+                  {/* <svg
                     className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
                     viewBox="0 0 1155 678"
                     fill="none"
@@ -264,7 +257,7 @@ const Banner = () => {
                         <stop offset={1} stopColor="#FF80B5" />
                       </linearGradient>
                     </defs>
-                  </svg>
+                  </svg> */}
                 </div>
               </div>
             </div>
