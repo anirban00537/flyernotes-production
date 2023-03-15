5556b416e7d34fcacc7620719a40723c1eb63dfa
anirban00537 <anirban00537@gmail.com>
2023-03-15 13:44:52 +0600

    responsive done

--- src/components/Landing/Banner/index.tsx
+++ src/components/Landing/Banner/index.tsx
@@ -4,7 +4,6 @@ import React from "react";
 const Banner = () => {
   return (
     <div>
-      {" "}
       <div className="isolate bg-white">
         <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
           {/* <svg
@@ -104,88 +103,6 @@ const Banner = () => {
                 </a>
               </div>
             </nav>
-            {/* Mobile menu, show/hide based on menu open state. */}
-            <div role="dialog" aria-modal="true">
-              <div
-                // focus="true"
-                className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden"
-              >
-                <div className="flex h-9 items-center justify-between">
-                  <div className="flex">
-                    <a href="#" className="-m-1.5 p-1.5">
-                      <span className="sr-only">Your Company</span>
-                      <img
-                        className="h-8"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=purple&shade=600"
-                        alt=""
-                      />
-                    </a>
-                  </div>
-                  <div className="flex">
-                    <button
-                      type="button"
-                      className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                    >
-                      <span className="sr-only">Close menu</span>
-                      {/* Heroicon name: outline/x-mark */}
-                      <svg
-                        className="h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <div className="mt-6 flow-root">
-                  <div className="-my-6 divide-y divide-gray-500/10">
-                    <div className="space-y-2 py-6">
-                      <a
-                        href="#"
-                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
-                      >
-                        Product
-                      </a>
-                      <a
-                        href="#"
-                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
-                      >
-                        Features
-                      </a>
-                      <a
-                        href="#"
-                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
-                      >
-                        Marketplace
-                      </a>
-                      <a
-                        href="#"
-                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
-                      >
-                        Company
-                      </a>
-                    </div>
-                    <div className="py-6">
-                      <a
-                        href="#"
-                        className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
-                      >
-                        Log in
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
         <main>
