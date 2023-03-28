047b727ae062f206e03d33fdcb14874e6e10749c
anirban00537 <anirban00537@gmail.com>
2023-03-28 16:13:49 +0600

    error solved

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -185,8 +185,6 @@ const index = () => {
                       className="h-12 w-auto mx-auto"
                       loading="lazy"
                       alt="client logo"
-                      width
-                      height
                     />
                   </div>
                   <div className="p-4 grayscale transition duration-200 hover:grayscale-0">
@@ -195,8 +193,6 @@ const index = () => {
                       className="h-12 w-auto mx-auto"
                       loading="lazy"
                       alt="client logo"
-                      width
-                      height
                     />
                   </div>
                   <div className="p-4 flex grayscale transition duration-200 hover:grayscale-0">
@@ -205,8 +201,6 @@ const index = () => {
                       className="h-9 w-auto m-auto"
                       loading="lazy"
                       alt="client logo"
-                      width
-                      height
                     />
                   </div>
                   <div className="p-4 grayscale transition duration-200 hover:grayscale-0">
@@ -215,8 +209,6 @@ const index = () => {
                       className="h-12 w-auto mx-auto"
                       loading="lazy"
                       alt="client logo"
-                      width
-                      height
                     />
                   </div>
                   <div className="p-4 flex grayscale transition duration-200 hover:grayscale-0">
@@ -225,8 +217,6 @@ const index = () => {
                       className="h-8 w-auto m-auto"
                       loading="lazy"
                       alt="client logo"
-                      width
-                      height
                     />
                   </div>
                   <div className="p-4 grayscale transition duration-200 hover:grayscale-0">
@@ -235,8 +225,6 @@ const index = () => {
                       className="h-12 w-auto mx-auto"
                       loading="lazy"
                       alt="client logo"
-                      width
-                      height
                     />
                   </div>
                 </div>
@@ -449,8 +437,6 @@ const index = () => {
                     src="./images/pie.svg"
                     alt="image"
                     loading="lazy"
-                    width
-                    height
                     className="w-full"
                   />
                 </div>
@@ -997,7 +983,6 @@ const index = () => {
                           width={16}
                           height={16}
                           fill="currentColor"
-                          className={5}
                           viewBox="0 0 16 16"
                         >
                           <path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z" />
